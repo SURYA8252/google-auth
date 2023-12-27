e7f89efc9ebc127c3af73121414c382248198c57
Surya jyoti <surya@sageite.com>
2023-12-27 10:49:46 +0530

    testing pull

--- src/main/java/com/google/auth/GoogleAuthApplication.java
+++ src/main/java/com/google/auth/GoogleAuthApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * 
+ * 
+ * @author Akash
+ *
+ */
 public class GoogleAuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleAuthApplication.class, args);
+		System.out.println("Pull Akash branch to surya branch...");
 	}
-
 }
